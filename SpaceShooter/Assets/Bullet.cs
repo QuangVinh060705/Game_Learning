a9aa4bf76b63b6c88672653f97f70bea74e7e76c
QuangVinh <quangvinh.dinhnguyen0607@gmail.com>
2026-01-22 14:09:51 +0700

    Assets for Space shooter

---
+++ SpaceShooter/Assets/Bullet.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float flySpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Update()
+        {
+        var newPosition = transform.position;
+        newPosition.y += Time.deltaTime * flySpeed;
+        transform.position = newPosition;
+        }
+}
