ca9bf4475ab3f17749cd0364de31a41102f76f03
QuangVinh <quangvinh.dinhnguyen0607@gmail.com>
2026-03-01 14:13:03 +0700

    Demo part 5

--- SpaceShooter/Assets/Bullet.cs
+++ SpaceShooter/Assets/Bullet.cs
@@ -4,6 +4,7 @@ public class Bullet : MonoBehaviour
     public float flySpeed;
     public int damage;
 
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var enemy = collision.GetComponent<EnemyHealth>();
